51e056e521293669dd8141e421bacf6f01cc1455
igordrangel <igor.rangel@ciadocredito.net.br>
2020-08-01 17:07:46 -0300

    commit

--- projects/ngx-koala/src/lib/shared/components/list/list.filter.interface.ts
+++ projects/ngx-koala/src/lib/shared/components/list/list.filter.interface.ts
@@ -1,4 +1,4 @@
-import { KoalaDynamicFormFieldInterface } from '../form/dynamic-form/interfaces/dynamic-form-field.interface';
+import { KoalaDynamicFormFieldInterface } from '../form/dynamic-form/interfaces/koala.dynamic-form-field.interface';
 
 export interface ListFilterInterface {
   main: KoalaDynamicFormFieldInterface[];
