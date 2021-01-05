0dbadc961f4647a9a7354027219d32fdc673ce07
Igor Rangel <igordrangel@gmail.com>
2021-01-05 11:56:41 -0300

    Incluindo tipo Observable ao KoalaRequestService

--- projects/ngx-koala/src/lib/shared/services/request/koala.request.service.ts
+++ projects/ngx-koala/src/lib/shared/services/request/koala.request.service.ts
@@ -1,3 +1,4 @@
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { KoalaRequestCodeToAlertEnumTranslate } from '../../components/alert/koala.request-code-to-alert-enum.translate';
 import { KoalaAlertService } from '../alert/koala.alert.service';
@@ -10,25 +11,45 @@ export class KoalaRequestService {
   }
 
   public request<T>(
-    request: Promise<T>,
+    request: Promise<T>|Observable<T>,
     success?: (response: T) => void,
     error?: (error: T) => void
   ) {
-    request.then((response) => {
-      if (success) {
-        success(response);
-      }
-    }).catch(e => {
-      if (error) {
-        error(e);
-      }
-      const alertEnum = KoalaRequestCodeToAlertEnumTranslate.translate(e.statusCode);
-      this.koalaAlertService.create({
-        alertEnum,
-        message: alertEnum === KoalaAlertEnum.internalServerError ?
-          'Ops, tivemos um problema com sua requisição.<br/>Sinto muito pelo transtorno.<br/>Gentileza tentar novamente mais tarde.' :
-          e.message
+    if (request instanceof Promise) {
+      request.then((response) => {
+        if (success) {
+          success(response);
+        }
+      }).catch(e => {
+        if (error) {
+          error(e);
+        }
+        this.showAlertError(e);
       });
+    } else {
+      return request.subscribe(
+        (response) => {
+          if (success) {
+            success(response);
+          }
+        },
+        (e) => {
+          if (error) {
+            error(e);
+          }
+          this.showAlertError(e);
+        }
+      );
+    }
+  }
+
+  private showAlertError(e: any) {
+    const alertEnum = KoalaRequestCodeToAlertEnumTranslate.translate(e.statusCode);
+    this.koalaAlertService.create({
+      alertEnum,
+      message: alertEnum === KoalaAlertEnum.internalServerError ?
+        'Ops, tivemos um problema com sua requisição.<br/>Sinto muito pelo transtorno.<br/>Gentileza tentar novamente mais tarde.' :
+        e.message
     });
   }
 }
