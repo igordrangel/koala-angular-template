8358028b8ce6861fc443c4a3a28bb24c64f09107
Igor Rangel <igordrangel@gmail.com>
2021-01-13 15:47:16 -0300

    commit

--- projects/ngx-koala/src/lib/shared/components/icon/icon.component.ts
+++ projects/ngx-koala/src/lib/shared/components/icon/icon.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { koala } from "koala-utils";
 
 @Component({
   selector: 'koala-icon',
@@ -10,21 +11,28 @@ export class IconComponent implements OnInit {
   @Input() icon: 'notFound'|'notAllowed'|'sessionExpired'|'empty'|'excel'|'edit'|'trash';
   @Input() size: number = 24;
 
+  public intervalCheckElement: number;
+  public style: string;
+  public idIcon = `koala-icons_${koala(0).number().random(1, 30).getValue()}`;
+
   ngOnInit() {
-    setTimeout(() => {
-      const elSvgIcon = document.querySelector(`svg#koala-icons.${this.icon}`) as SVGElement;
-      elSvgIcon.style.width = `${this.size}px`;
-      elSvgIcon.style.height = `${this.size}px`;
-      document.querySelectorAll(`
-        #koala-icons path,
-        #koala-icons rect,
-        #koala-icons circle,
-        #koala-icons polygon
-      `).forEach((el: SVGPathElement) => {
-        if (!el.style.fill) {
-          el.style.fill = this.color;
-        }
-      });
+    this.style = `width: ${this.size}px;height: ${this.size}px;`;
+    this.intervalCheckElement = setInterval(() => {
+      const mainElementSelector = `svg#${this.idIcon}.${this.icon}`;
+      const elSvgIcon = document.querySelector(mainElementSelector) as SVGElement;
+      if (elSvgIcon) {
+        document.querySelectorAll(`
+          ${mainElementSelector} path,
+          ${mainElementSelector} rect,
+          ${mainElementSelector} circle,
+          ${mainElementSelector} polygon
+        `).forEach((el: SVGPathElement) => {
+          if (!el.style.fill) {
+            el.style.fill = this.color;
+          }
+        });
+        clearInterval(this.intervalCheckElement);
+      }
     }, 1);
   }
 }
