4291d371d2cc8ebefc8e43c6ba6d2856c802b9dc
Igor Rangel <igordrangel@gmail.com>
2021-01-27 15:01:14 -0300

    Breaking Change
    - Atributos no componente koala-list foram substituídos por config;
    - Criado builder do config list e implementado no serviço KoalaListService

--- projects/koala-template/src/app/components/list/page-list.component.ts
+++ projects/koala-template/src/app/components/list/page-list.component.ts
@@ -2,94 +2,103 @@ import { Component } from "@angular/core";
 import { PageAbstract } from "../../shared/abstract/page.abstract";
 import { ListService } from "./list.service";
 import { FormGroup } from "@angular/forms";
-import { KoalaListItemInterface } from "../../../../../ngx-koala/src/lib/shared/components/list/koala-list-item.interface";
-import { KoalaListFilterInterface } from "../../../../../ngx-koala/src/lib/shared/components/list/koalaListFilterInterface";
-import { KoalaListItemMenuOptionInterface } from "../../../../../ngx-koala/src/lib/shared/components/list/koala-list-item-menu-option.interface";
-import { BehaviorSubject } from "rxjs";
-import { KoalaListFormFilterInterface } from "../../../../../ngx-koala/src/lib/shared/components/list/koala-list-form-filter.interface";
-import { SelectionModel } from "@angular/cdk/collections";
 import { ListItemInterface } from "./list-item.interface";
 import { koala } from "koala-utils";
 import { DynamicFormTypeFieldEnum } from "../../../../../ngx-koala/src/lib/shared/components/form/dynamic-form/enums/dynamic-form-type-field.enum";
-import { KoalaDynamicComponent } from "../../../../../ngx-koala/src/lib/shared/components/dynamic-component/koala-dynamic-component";
 import { EmptyListComponent } from "./empty-list/empty-list.component";
 import { KoalaXlsxService } from "../../../../../ngx-koala/src/lib/shared/services/xlsx/koala.xlsx.service";
+import { KoalaListConfigInterface } from "../../../../../ngx-koala/src/lib/shared/components/list/koala.list-config.interface";
+import { KoalaListService } from "../../../../../ngx-koala/src/lib/shared/services/list/koala.list.service";
+import { SelectionModel } from "@angular/cdk/collections";
+import { KoalaAlertService } from "../../../../../ngx-koala/src/lib/shared/services/alert/koala.alert.service";
+import { KoalaAlertEnum } from "../../../../../ngx-koala/src/lib/shared/components/alert/koala.alert.enum";
 
 @Component({
   templateUrl: 'page-list.component.html'
 })
 export class PageListComponent extends PageAbstract {
   public formData: FormGroup;
-  public columns = [
-    'select',
-    'name',
-    'qtd',
-    'value',
-    'options'
-  ];
-  public itensList: KoalaListItemInterface[];
-  public itensMenuListOptions: KoalaListItemMenuOptionInterface[];
-  public filterConfig: KoalaListFilterInterface;
-  public filter = new BehaviorSubject<KoalaListFormFilterInterface>({
-    params: {},
-    sort: 'name',
-    order: 'asc',
-    page: 1,
-    limit: 0
-  });
-  public selection: SelectionModel<ListItemInterface>;
   public dataSource?: ListItemInterface[];
-  public emptyListComponent = new KoalaDynamicComponent(EmptyListComponent);
-  public reloadList = new BehaviorSubject<boolean>(false);
+  public config: KoalaListConfigInterface;
+  private selectedItems?: SelectionModel<ListItemInterface>;
 
   constructor(
     public listService: ListService,
+    private koalaListService: KoalaListService,
+    private alertService: KoalaAlertService,
     private xlsxService: KoalaXlsxService
   ) {
     super();
-    this.itensList = [{
-      label: 'Name',
-      columnDef: 'name',
-      sortHeader: 'name',
-      itemNameProperty: (item: ListItemInterface) => item.name,
-      dblClick: (item: ListItemInterface) => this.edit(item)
-    }, {
-      label: 'Qtd.',
-      columnDef: 'qtd',
-      sortHeader: 'qtd',
-      itemNameProperty: (item: ListItemInterface) => item.qtd.toString(),
-      dblClick: (item: ListItemInterface) => this.edit(item)
-    }, {
-      label: 'Value',
-      columnDef: 'value',
-      sortHeader: 'value',
-      itemNameProperty: (item: ListItemInterface) => koala(item.qtd).number().maskCoin('US$ ').getValue(),
-      dblClick: (item: ListItemInterface) => this.edit(item)
-    }];
-
-    this.itensMenuListOptions = [{
-      icon: 'edit',
-      name: 'Edit',
-      havePermission: true,
-      action: (item: ListItemInterface) => this.edit(item)
-    }];
-
-    this.filterConfig = {
-      main: [
-        {
-          label: 'Name',
-          name: 'name',
-          type: DynamicFormTypeFieldEnum.text,
-          appearance: 'outline',
-          floatLabel: "always",
-          class: 'col-12'
-        }
-      ]
-    };
+    this.config = this.koalaListService
+                      .build<ListItemInterface>()
+                      .service(this.listService.getList())
+                      .filterConfig({
+                        main: [{
+                          label: 'Name',
+                          name: 'name',
+                          type: DynamicFormTypeFieldEnum.text,
+                          appearance: 'outline',
+                          floatLabel: "always",
+                          class: 'col-12'
+                        }]
+                      })
+                      .defaultFilter({
+                        params: {},
+                        sort: 'name',
+                        order: 'asc',
+                        page: 1,
+                        limit: 0
+                      })
+                      .columns([
+                        'select',
+                        'name',
+                        'qtd',
+                        'value',
+                        'options'
+                      ])
+                      .itemColumn({
+                        label: 'Name',
+                        columnDef: 'name',
+                        sortHeader: 'name',
+                        itemNameProperty: (item: ListItemInterface) => item.name,
+                        dblClick: (item: ListItemInterface) => this.edit(item)
+                      })
+                      .itemColumn({
+                        label: 'Qtd.',
+                        columnDef: 'qtd',
+                        sortHeader: 'qtd',
+                        itemNameProperty: (item: ListItemInterface) => item.qtd.toString(),
+                        dblClick: (item: ListItemInterface) => this.edit(item)
+                      })
+                      .itemColumn({
+                        label: 'Value',
+                        columnDef: 'value',
+                        sortHeader: 'value',
+                        itemNameProperty: (item: ListItemInterface) => koala(item.qtd).number().maskCoin('US$ ').getValue(),
+                        dblClick: (item: ListItemInterface) => this.edit(item)
+                      })
+                      .actionList({
+                        icon: 'edit',
+                        name: 'Edit',
+                        havePermission: true,
+                        action: (item: ListItemInterface) => this.edit(item)
+                      })
+                      .emptyListComponent(EmptyListComponent)
+                      .getDataSource(dataSource => this.dataSource = dataSource)
+                      .getSelectionList(selection => this.selectedItems = selection)
+                      .getConfig();
   }
 
   public edit(item: ListItemInterface) {
     console.log(item);
+  }
+
+  public delete() {
+    console.log(this.selectedItems.selected);
+    this.alertService.create({
+      alertEnum: KoalaAlertEnum.success,
+      message: 'The selected items was showed in devTools console. Press F12 to view.'
+    })
   }
 
   public downloadList() {
