c506650672bee2b43b91d00e44dd02ac72743735
Igor Rangel <igordrangel@gmail.com>
2021-01-28 17:08:46 -0300

    commit

--- projects/ngx-koala/src/lib/shared/services/dynamic-forms/builder/fields/field-select.builder.ts
+++ projects/ngx-koala/src/lib/shared/services/dynamic-forms/builder/fields/field-select.builder.ts
@@ -1,7 +1,7 @@
 import { FieldSelectBase } from "./field-select.base";
 import { DynamicFormTypeFieldEnum } from "../../../../components/form/dynamic-form/enums/dynamic-form-type-field.enum";
-import { KoalaDynamicFormConfigInterface } from "../koala.dynamic-form-config.interface";
 import { FormBuilder } from "@angular/forms";
+import { KoalaDynamicFormConfigInterface } from "../../../../components/form/dynamic-form/interfaces/koala.dynamic-form-config.interface";
 
 export class FieldSelectBuilder extends FieldSelectBase {
 
