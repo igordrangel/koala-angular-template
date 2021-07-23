88177d4c86ce0ce70b703d5c55a654b6742511c8
Igor Rangel <igordrangel@gmail.com>
2021-07-23 14:50:40 -0300

    commit

--- projects/koala-template/src/environments/environment.prod.ts
+++ projects/koala-template/src/environments/environment.prod.ts
@@ -1,4 +1,14 @@
 export const environment = {
   production: true,
-  storageTokenName: 'koalaStorageToken'
+  oauthConfig: [{
+    name: 'google',
+    customQueryParams: {
+      client_secret: 't1gbmEgh4EUgAJRLy16Zjrks'
+    },
+    clientId: '679440572859-iviegii370t3n4m15qrpr4fpj4db8jc7.apps.googleusercontent.com',
+    scope: 'openid profile email',
+    domain: 'https://accounts.google.com',
+    strictDiscoveryDocumentValidation: false,
+    indexLoginName: 'name'
+  }]
 };
