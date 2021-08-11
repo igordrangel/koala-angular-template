33b04c1c458af044096aa2108f1477f0cbf5d7ce
Igor Rangel <igordrangel@gmail.com>
2021-08-11 14:28:22 -0300

    commit

--- projects/koala-template/src/app/app.component.ts
+++ projects/koala-template/src/app/app.component.ts
@@ -23,74 +23,74 @@ export class AppComponent {
     icon: 'get_app',
     name: 'Get Started',
     routerLink: '/get-started'
-  },{
+  }, {
     icon: 'menu_book',
     name: 'Guides',
     tools: [{
       name: 'CRUD Page',
       routerLink: '/guides/crud-page'
     }]
-  },{
+  }, {
     icon: 'webComponents',
     koalaIcon: true,
     name: 'Components',
     tools: [{
       name: 'Button',
       routerLink: '/components/button'
-    },{
+    }, {
       name: 'Dialog',
       routerLink: '/components/dialog'
-    },{
+    }, {
       name: 'File Button',
       routerLink: '/components/file-button'
-    },{
+    }, {
       name: 'Folder Page',
       routerLink: '/components/folder-page'
-    },{
+    }, {
       name: 'Form',
       routerLink: '/components/dynamic-form'
-    },{
+    }, {
       name: 'Icons',
       routerLink: '/components/icons'
-    },{
+    }, {
       name: 'Icons Animated',
       routerLink: '/components/icons-animated'
-    },{
+    }, {
       name: 'List',
       routerLink: '/components/list'
     }]
-  },{
+  }, {
     icon: 'deliveryBox',
     koalaIcon: true,
     name: 'Services',
     tools: [{
       name: 'Alert',
       routerLink: '/services/alert'
-    },{
+    }, {
       name: 'CSV',
       routerLink: '/services/csv'
-    },{
+    }, {
       name: 'Menu',
       routerLink: '/services/menu'
-    },{
+    }, {
       name: 'OpenID',
       routerLink: '/services/oauth2'
-    },{
+    }, {
       name: 'Page Loader',
       routerLink: '/services/page-loading'
-    },{
+    }, {
       name: 'Question',
       routerLink: '/services/question'
-    },{
+    }, {
       name: 'Request',
       routerLink: '/services/request'
-    },{
+    }, {
       name: 'Snackbar',
       routerLink: '/services/snackbar'
-    },{
+    }, {
       name: 'Token',
       routerLink: '/services/token'
-    },{
+    }, {
       name: 'XLSX',
       routerLink: '/services/xlsx'
     }]
--- projects/ngx-koala/src/lib/shared/components/notifications/notification.component.ts
+++ projects/ngx-koala/src/lib/shared/components/notifications/notification.component.ts
@@ -12,26 +12,26 @@ export class NotificationComponent implements OnChanges {
   @Input() notifications: KoalaNotificationInterface[];
   @Output() deleteAll = new EventEmitter<boolean>(false);
   @Output() delete = new EventEmitter<KoalaNotificationInterface>(null);
-  
+
+  private appName: string;
+
   constructor(private titleService: Title) {}
-  
+
   ngOnChanges(changes: SimpleChanges) {
-    const arrAppName = this.titleService.getTitle().split(' ');
-    let appName = arrAppName[0];
-    if (arrAppName.length > 1) {
-      appName = arrAppName[1];
+    if (!this.appName) {
+      this.appName = this.titleService.getTitle();
     }
     if (changes.notifications && this.notifications.length > 0) {
-      this.titleService.setTitle(`(${this.notifications.length}) ${appName}`);
+      this.titleService.setTitle(`(${this.notifications.length}) ${this.appName}`);
     } else {
-      this.titleService.setTitle(`${appName}`);
+      this.titleService.setTitle(this.appName);
     }
   }
-  
+
   public deleteAllNotifications() {
     this.deleteAll.emit(true);
   }
-  
+
   public deleteNotification(notification: KoalaNotificationInterface) {
     this.delete.emit(notification);
   }
